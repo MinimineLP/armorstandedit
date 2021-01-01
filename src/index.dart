405c5dd81fa6f6f04fff73ec55fc7936013fc264
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2021-01-01 18:47:52 +0100

    beautified description in index.dart

--- src/index.dart
+++ src/index.dart
@@ -1,4 +1,3 @@
-
 import 'package:objd/core.dart';
 import 'packs/armor_stands/pack.dart';
 
@@ -10,7 +9,10 @@ void main(List<String> args) {
     Project(
       name: 'ArmorStandEdit',
       target: "./",
-      description: "§7§oArmorStandEdit is an armor stand editor for minecraft to give players that don't have access to commands or dont like to use them access to the world of beautiful armor stand statues§r\n§3by Miniminine§r",
+      description: "§7§oArmorStandEdit is an armor stand editor for minecraft to"
+          " give players that don't have access to commands or dont like to use "
+          "them access to the world of beautiful armor stand statues§r\n"
+          "§3by Miniminine§r",
       generate: ArmorStandPack(),
     ),
     args
